acedf370ccebb5c960004f8de8f66ee15199fa45
Yurii <yuriygorelkin@gmail.com>
2020-08-04 22:21:13 +0300

    added adaptive to fairy-tail section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-$(function(){
-$('.fairy-tail__slider, .our-trip__slider').slick({
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow prew"></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow next"></button>',
-    autoplay: true,
-    fade:true
-});
+$(function () {
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow prew"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow next"></button>',
+        autoplay: true,
+        fade: true,
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false
+            }
+        },]
+    });
 });
