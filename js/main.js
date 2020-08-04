c0646bec74e279d5546b5218719d5cc162a1a54a
Yurii <yuriygorelkin@gmail.com>
2020-08-04 13:34:58 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+$('.fairy-tail__slider, .our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow prew"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow next"></button>',
+    autoplay: true,
+    fade:true
+});
+});
